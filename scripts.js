a793f545e3ead74b63804acfee803772bfcbf515
Leilane Turini <l.turini95@gmail.com>
2022-10-25 18:05:42 -0300

    projeto calculadora

--- scripts.js
+++ scripts.js
@@ -3,13 +3,62 @@ const operationButtons = document.querySelectorAll("[data-operador]");
 const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
 const AllClearButton = document.querySelector("[data-all-clear]");
-const previousOpTextElement = document.querySelector("[data-previous]");
-const currentOpTextElement = document.querySelector("[data-current]");
+const previousOpTextElement = document.querySelector("[data-previous-operand]");
+const currentOpTextElement = document.querySelector("[data-current-operand]");
+
 
 class Calculator {
   constructor(previousOpTextElement, currentOpTextElement) {
     this.previousOpTextElement = previousOpTextElement;
     this.currentOpTextElement = currentOpTextElement;
+    this.clear()
+  }
+
+  calculate() {
+    let result;
+
+    const _previousOperand= parseFloat(this.previousOperand);
+    const _currentOperand = parseFloat(this.currentOperand);
+
+    if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+
+    switch (this.operation) {
+      case "+":
+        result = _previousOperand + _currentOperand;
+        break;
+      case "-":
+        result = _previousOperand - _currentOperand;
+        break;
+        case "÷":
+        result = _previousOperand / _currentOperand;
+        break;
+      case "*":
+        result = _previousOperand * _currentOperand;
+        break;
+    
+      default:
+        return;
+    }
+    this.currentOperand = result;
+    this.operation = undefined
+    this.previousOperand =''
+  }
+
+  chooseOperation(operation) {
+    if( this.currentOperand ==="") return;
+    if (this.previousOperand !== "") {
+      this.calculate();
+    }
+
+    this.operation = operation;
+
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+  appendNumber(number) {
+    if (this.currentOperand.includes(".") && number === ".") return;
+
+    this.currentOperand = `${this.currentOperand} ${number.toString()}`;
   }
 
   clear() {
@@ -18,13 +67,33 @@ class Calculator {
     this.operation = undefined;
   }
   updateDisplay() {
-    this.previousOpTextElement.innerText = this.previousOperand;
+    this.previousOpTextElement.innerText = `${this.previousOperand} ${
+      this.operation || ""
+    }`;
     this.currentOpTextElement.innerText = this.currentOperand;
   }
 }
 const calculator = new Calculator(previousOpTextElement, currentOpTextElement);
 
+for (const numberButton of numberButtons) {
+  numberButton.addEventListener("click", () => {
+    calculator.appendNumber(numberButton.innerText);
+    calculator.updateDisplay();
+  });
+}
+
+for (const operationButton of operationButtons) {
+  operationButton.addEventListener("click", () => {
+    calculator.chooseOperation(operationButton.innerText);
+    calculator.updateDisplay();
+  });
+}
 AllClearButton.addEventListener("click", () => {
   calculator.clear();
   calculator.updateDisplay();
 });
+
+equalsButton.addEventListener('click', ()=>{
+  calculator.calculate()
+  calculator.updateDisplay()
+})
