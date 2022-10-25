ed6c10a8ee7df2aa977d3b663d7a48848652fda8
Leilane Turini <l.turini95@gmail.com>
2022-10-24 23:45:30 -0300

    projeto calculadora

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operador]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const AllClearButton = document.querySelector("[data-all-clear]");
+const previousOpTextElement = document.querySelector("[data-previous]");
+const currentOpTextElement = document.querySelector("[data-current]");
+
+class calculator {
+  constructor(previousOpTextElement, currentOpTextElement) {
+    this.previousOpTextElement = previousOpTextElement;
+    this.currentOpTextElement = currentOpTextElement;
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+}
